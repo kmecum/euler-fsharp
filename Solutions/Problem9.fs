a362c0c5578c0e75c30bec6640fa55a7220427d2
Kenneth Mecum <k@kenmecum.com>
2018-11-27 16:18:33 -0700

    small changes to constants on problem 9

--- Solutions/Problem9.fs
+++ Solutions/Problem9.fs
@@ -2,8 +2,8 @@ namespace Solutions
 
 module Problem9 =
     let solve () : int64 =
-        let list = [for a in 1L..333L do
-                    for b in 1L..500L do
+        let list = [for a in 1L..331L do
+                    for b in 2L..499L do
                     for c in 3L..997L do
                         if a + b + c = 1000L then
                             yield (a, b, c)]
